be122e9194335d878b6cc218f9cfa94722d29702
Your Name <you@example.com>
2016-04-09 08:07:53 -0700

    refactored-files

---
+++ public/scripts/projects.js
@@ -0,0 +1,30 @@
+var bloccitProject ={
+	title: "Bloccit",
+	descriptions: [
+		"Bloccit is a site I build to learn back end web development Fundamentals",
+		"Implementation of Database modeling for create, read, update, and delete",
+		"implementation of AWS cloud Services"
+		],
+    pics:['/assets/images/bloccit/bloccitHome.gif','/assets/images/bloccit/bloccit-formsCRUD.gif','/assets/images/bloccit/bloccitcontrols.gif']
+	}
+
+var blocmetricsProject = {
+	title:"Blocmetrics",
+	descriptions:[
+		"API Tacking system",
+		"Visitors Time Graphing "
+		
+		],
+	pics:['/assets/images/blocmetric/blocmetrichome.gif','/assets/images/blocmetric/blocmetricgraph.gif']
+}
+
+var resume = {
+    name:"Hi, I'm Miguel",
+    objective:"Web applications developer with a Background in Rails, Node, PHP, Javascript, CSS, and HTML5, I can work on the frontend and backend of a web application and eager to learn other languages and stacks as well!",
+    
+    qualities:"I believe in adding to the wheel not reinventing it, lets move forward not backward, Learning the newest languages is the only  smart thing to do, though it  may get frustrating at times I understand patience is the tool for focusing and understanding the details of the puzzle. I work hard on learning what others think is too hard and research everything I think will help be productive in the development world. I don’t just code for a job, I push to be a fierce competitor in the programming world. All I got to say to the world is ‘look out world, I’m coming!!’. ",
+    
+    experience:"Two years in Web Development and Training.<br> Two years ago I started out with building simple html pages just for fun, then got really interested in php, so I did various works on php and sql, felt like I wasnt getting the whole picture so I enrolled in Bloc.io to become a fullstack web developer",
+    
+    education:"Blocs' FullStack Web Developer Training"
+        }
