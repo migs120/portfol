92839c18b1edebdd36c1ba31b6c30dc9ca954a95
migs120 <migs432@yahoo.com>
2020-08-28 17:42:32 -0700

    {revert_toOld}

--- public/scripts/projects.js
+++ public/scripts/projects.js
@@ -9,7 +9,7 @@ var bloccitProject ={
 	}
 
 var blocmetricsProject = {
-	title:"Blocmetrics ",
+	title:"Blocmetrics",
 	descriptions:[
 		"API Tacking system",
 		"Visitors Time Graphing "
@@ -20,11 +20,11 @@ var blocmetricsProject = {
 
 var resume = {
     name:"Hi, I'm Miguel",
-    objective:"Web applications developer with a Background in Rails, Node, Javascript, React and Angular 7, I can work on the frontend and backend of a web application and eager to learn other languages and stacks as well!",
+    objective:"Web applications developer with a Background in Rails, Node, PHP, Javascript, CSS, and HTML5, I can work on the frontend and backend of a web application and eager to learn other languages and stacks as well!",
     
     qualities:"I believe in adding to the wheel not reinventing it, let's move forward not backward, Learning the newest languages is the only smart thing to do, though it may get frustrating at times I understand patience is the tool for focusing and understanding the details of the puzzle. I work hard on learning what others think is too hard and research everything I think will help be productive in the development world. I don't just code for a job, I push to be a fierce competitor in the programming world. All I have to say to the world is ‘look out world, I'm coming!!'. ",
     
-    experience:"Seven years in Web Development.<br> Seven years ago I started out with building simple html pages just for fun, then got really interested in php, so I did various works on php and sql, felt like I wasnt getting the whole picture so I enrolled in Bloc.io to become a fullstack web developer",
+    experience:"Five years in Web Development.<br> Five years ago I started out with building simple html pages just for fun, then got really interested in php, so I did various works on php and sql, felt like I wasnt getting the whole picture so I enrolled in Bloc.io to become a fullstack web developer",
     
     education:"Blocs' FullStack Web Developer Training"
         }
